03959a9381b8727607fdf6ce959321b14946bdc8
PrivatecharterX <info@privatecharterx.com>
2025-10-05 14:23:01 +0300

    Fix phone mockup - shadow touches bottom edge

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -96,8 +96,8 @@ export default function Hero(){
           </div>
 
           {/* Right Column - Phone Mockup Animation */}
-          <div className="hidden lg:flex justify-center items-end relative min-h-[500px]">
-            <div className="absolute" style={{ bottom: '-60px', transform: 'scale(0.95)', transformOrigin: 'bottom center' }}>
+          <div className="hidden lg:flex justify-center items-end relative">
+            <div style={{ transform: 'scale(0.95)', transformOrigin: 'bottom center', marginBottom: '-20px' }}>
               <AnimatedPhoneMockup />
             </div>
           </div>
